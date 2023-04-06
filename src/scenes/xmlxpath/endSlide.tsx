558953d227d6000e3696df3aeeca7897a42a85d7
NiklasNeudorfer <nikitrash93@gmail.com>
2023-04-06 07:24:39 +0200

    xml/xpath - end slide remove smile rotation

--- src/scenes/xmlxpath/endSlide.tsx
+++ src/scenes/xmlxpath/endSlide.tsx
@@ -1,29 +1,32 @@
 import {makeScene2D} from "@motion-canvas/2d";
 import {beginSlide, createRef} from "@motion-canvas/core/lib/utils";
-import {Circle, Img, Node, Polygon, Rect, Spline, Txt, Video} from "@motion-canvas/2d/lib/components";
-import {all, chain, sequence, waitFor} from "@motion-canvas/core/lib/flow";
+import {Circle, Node, Rect, Spline, Txt} from "@motion-canvas/2d/lib/components";
+import {all, sequence} from "@motion-canvas/core/lib/flow";
 import {slideTransition} from "@motion-canvas/core/lib/transitions";
 import {Direction} from "@motion-canvas/core/lib/types";
-import {easeInBounce, easeOutCubic, SmoothSpring, spring} from "@motion-canvas/core/lib/tweening";
+import {easeOutCubic, SmoothSpring, spring} from "@motion-canvas/core/lib/tweening";
 
 export default makeScene2D(function* (view) {
+
+    view.fill("black")
 
     const spline = createRef<Spline>();
     const bottomBrow = createRef<Node>()
     const topBrow = createRef<Node>()
     const eyes = createRef<Node>()
+    const endScreen = createRef<Node>()
 
     const text = createRef<Txt>()
 
     view.add(
-        <>
+        <Node ref={endScreen}>
             <Txt ref={text} fill={"white"}
                  fontSize={120}
                  fontFamily={"Fira Code"}
                  y={() => view.height() / 2 - 100}
                  x={() => view.width() / -2 + 950}></Txt>
 
-            <Node rotation={5}>
+            <Node>
 
                 <Spline
                     ref={spline}
@@ -46,8 +49,8 @@ export default makeScene2D(function* (view) {
 
 
                 <Node ref={topBrow} y={-120}>
-                    <Rect fill={"black"} height={120} width={600} x={400} rotation={-20} y={-240}></Rect>
-                    <Rect fill={"black"} height={120} width={600} x={-400} rotation={20} y={-240}></Rect>
+                    <Rect fill={"black"} height={120} width={300} x={300} rotation={-20} y={-190}></Rect>
+                    <Rect fill={"black"} height={120} width={300} x={-300} rotation={20} y={-190}></Rect>
                 </Node>
 
                 <Node ref={bottomBrow} y={120}>
@@ -56,8 +59,10 @@ export default makeScene2D(function* (view) {
                 </Node>
 
             </Node>
-        </>
+        </Node>
     );
+
+    yield* slideTransition(Direction.Bottom, 2)
 
     yield* sequence(
         0.3,
@@ -70,6 +75,8 @@ export default makeScene2D(function* (view) {
 
     yield* beginSlide("eyes sqinch")
     yield* all(
+        //endScreen().scale(2,2),
+        //endScreen().position.y(120,2),
         topBrow().position.y(0, 1),
         bottomBrow().position.y(0, 1)
     )

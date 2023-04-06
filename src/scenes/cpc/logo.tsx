5c6e7b342614c1e0d049fc1bf30e8ac41561a405
NiklasNeudorfer <nikitrash93@gmail.com>
2023-04-06 07:23:35 +0200

    cpc - init project

---
+++ src/scenes/cpc/logo.tsx
@@ -0,0 +1,10 @@
+import {makeScene2D} from "@motion-canvas/2d";
+import {CodeBlock, insert, lines} from "@motion-canvas/2d/lib/components/CodeBlock";
+import {beginSlide, createRef} from "@motion-canvas/core/lib/utils";
+import {slideTransition} from "@motion-canvas/core/lib/transitions";
+import {Direction} from "@motion-canvas/core/lib/types";
+import {all, chain} from "@motion-canvas/core/lib/flow";
+
+export default makeScene2D(function* (view) {
+
+})

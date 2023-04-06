22715436a39f6b58a8775241f77a4c8ee55fd525
NiklasNeudorfer <nikitrash93@gmail.com>
2023-04-06 19:30:51 +0200

    cpc - title enomics logo

--- src/cpc-project.ts
+++ src/cpc-project.ts
@@ -1,6 +1,8 @@
 import {makeProject} from "@motion-canvas/core";
 import logo from "./scenes/cpc/logo?scene";
+import uiComparison from "./scenes/cpc/ui-comparison?scene";
+import "./global.css";
 
 export default makeProject({
-    scenes: [logo],
+    scenes: [logo, uiComparison],
 });
--- src/scenes/cpc/logo.tsx
+++ src/scenes/cpc/logo.tsx
@@ -4,47 +4,62 @@ import {beginSlide, createRef} from "@motion-canvas/core/lib/utils";
 import {slideTransition} from "@motion-canvas/core/lib/transitions";
 import {Direction} from "@motion-canvas/core/lib/types";
 import {all, chain, waitFor} from "@motion-canvas/core/lib/flow";
-import {Img, Rect, Txt} from "@motion-canvas/2d/lib/components";
+import {Img, Rect, Txt, Node} from "@motion-canvas/2d/lib/components";
 import enomicsFont from "../../../images/cpc/EnomicsFont.jpg"
 
 export default makeScene2D(function* (view) {
 
-    view.fill("white")
-
-    view.add(
-        <Txt fontFamily={"Fira Code"} text={"Charging Point Controller"}/>
-    )
-
+    const bg = "white"
+    const enomicsColor = "rgb(0, 187, 255)"
     const logoLength = 500;
 
-    view.add(
-        <>
-            <Img src={enomicsFont} scale={10} opacity={0}/>
+    view.fill(bg)
 
-            <Rect height={logoLength} width={logoLength}
-                  fill={"cyan"}
-                  opacity={1}
+    const enomics = createRef<Node>()
+
+    view.add(
+        <Rect fill={"black"} width={() => view.width() * 2} height={() => view.height() * 2}>
+
+            <Rect fill={"white"} radius={70} smoothCorners
+                  width={() => view.width()} height={() => view.height()}>
+            </Rect>
+        </Rect>
+    )
+
+    view.add(
+        <Node ref={enomics} scale={0.5} x={125 - 230}>
+            <Img src={enomicsFont} scale={10.2} opacity={0}/>
+
+            <Txt x={-420} fill={enomicsColor} y={30} fontSize={390} fontFamily={"Open Sans"} text={"EN"}></Txt>
+            <Txt x={630} fill={enomicsColor} y={30} fontSize={390} fontFamily={"Open Sans"} text={"MICS"}></Txt>
+
+
+            <Rect zIndex={-100} height={logoLength} width={logoLength}
+                  fill={enomicsColor}
+                  scale={0.57}
+                // opacity={1}
+                // x={-410}
                   radius={130}
-                  smoothCorners
-                  x={-405}>
+                  smoothCorners>
 
                 <Rect height={logoLength} width={logoLength}
-                      fill={"white"}
+                      fill={bg}
                       scale={0.71}
                       opacity={1}
-                      radius={130}
+                      clip
+                      radius={90}
                       smoothCorners>
                 </Rect>
 
                 <Rect height={50}
-                      width={logoLength}
-                      fill={"white"}/>
+                      width={logoLength + 20}
+                      fill={bg}/>
 
                 <Rect rotation={90} height={50}
-                      width={logoLength}
-                      fill={"white"}/>
+                      width={logoLength + 20}
+                      fill={bg}/>
             </Rect>
-        </>
+        </Node>
     )
 
 

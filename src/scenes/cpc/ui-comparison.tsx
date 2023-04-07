2d218de3c44acbfe62e89107e605f6d82c5806e5
NiklasNeudorfer <nikitrash93@gmail.com>
2023-04-07 17:19:55 +0200

    cpc - centered old UI img in ui-comparison

--- src/scenes/cpc/ui-comparison.tsx
+++ src/scenes/cpc/ui-comparison.tsx
@@ -1,12 +1,11 @@
 import {makeScene2D} from "@motion-canvas/2d";
 import {slideTransition} from "@motion-canvas/core/lib/transitions";
 import {Direction} from "@motion-canvas/core/lib/types";
-import {all, chain, sequence, waitFor} from "@motion-canvas/core/lib/flow";
-import {Img, Knot, Node, Rect, Spline, Txt} from "@motion-canvas/2d/lib/components";
+import {all, sequence} from "@motion-canvas/core/lib/flow";
+import {Img, Rect, Txt} from "@motion-canvas/2d/lib/components";
 import {beginSlide, createRef} from "@motion-canvas/core/lib/utils";
 import oldUI from "../../../images/cpc/old-gui.png"
 import newUI from "../../../images/cpc/overview_ui.png"
-import {createSignal} from "@motion-canvas/core/lib/signals";
 
 export default makeScene2D(function* (view) {
 
@@ -30,8 +29,10 @@ export default makeScene2D(function* (view) {
     view.add(
         <>
             <Txt fontFamily={"Open Sans"} fontSize={120} ref={titleTxt} text={"UI Comparison"}></Txt>
-            <Img ref={oldUIImage} scale={1} y={()=> view.height() * 2} shadowBlur={50} shadowColor={"black"} radius={30} clip src={oldUI}/>
-            <Img zIndex={20} ref={newUIImage} scale={1} y={()=> view.height()} shadowColor={enomicsColor} radius={30} clip src={newUI}/>
+            <Img ref={oldUIImage} scale={1} x={() => view.height()} y={() => view.height() * 2} shadowBlur={50}
+                 shadowColor={"black"} radius={30} clip src={oldUI}/>
+            <Img zIndex={20} ref={newUIImage} scale={1} y={() => view.height()} shadowColor={enomicsColor} radius={30}
+                 clip src={newUI}/>
         </>
     )
 
@@ -42,19 +43,25 @@ export default makeScene2D(function* (view) {
     yield* beginSlide("Start")
 
 
-    yield* all(
-        oldUIImage().position.y(0,1),
-        oldUIImage().position.x(-300,1)
+    yield* sequence(
+        1,
+        all(
+            oldUIImage().position.y(0, 2),
+            oldUIImage().position.x(0, 2)
+        ),
+        oldUIImage().scale(1.1, 1)
     )
 
     yield* beginSlide("New UI")
 
     yield* sequence(
         0.3,
-        newUIImage().position.y(0,3),
-        oldUIImage().position.x(0,2),
-        newUIImage().scale(1.2,3),
-        newUIImage().shadowBlur(100,2).to(30,1).to(100,1).to(30,1).to(100,1)
+        newUIImage().position.y(0, 3),
+
+        oldUIImage().scale(1, 3),
+
+        newUIImage().scale(1.2, 3),
+        newUIImage().shadowBlur(100, 2).to(30, 1).to(100, 1).to(30, 1).to(100, 1)
     )
 
     yield* beginSlide("End UI")

737e42a709f6971e8b99e79d135a319cb21fc202
NiklasNeudorfer <nikitrash93@gmail.com>
2023-04-07 21:23:36 +0200

    cpc - enhanced title slide

--- src/scenes/cpc/title.tsx
+++ src/scenes/cpc/title.tsx
@@ -3,6 +3,7 @@ import {beginSlide, createRef} from "@motion-canvas/core/lib/utils";
 import {all, chain, sequence} from "@motion-canvas/core/lib/flow";
 import {Rect, Txt} from "@motion-canvas/2d/lib/components";
 import {SmoothSpring, spring} from "@motion-canvas/core/lib/tweening";
+import {createSignal} from "@motion-canvas/core/lib/signals";
 
 export default makeScene2D(function* (view) {
 
@@ -17,7 +18,7 @@ export default makeScene2D(function* (view) {
     view.add(
         <Rect fill={"black"} width={() => view.width()} height={() => view.height()}>
 
-            <Rect ref={bgRef} fill={"white"} radius={bgRadius} smoothCorners
+            <Rect ref={bgRef} fill={bg} radius={bgRadius} smoothCorners
                   width={() => view.width()} height={() => view.height()}>
             </Rect>
         </Rect>
@@ -32,15 +33,32 @@ export default makeScene2D(function* (view) {
     const secondText = createRef<Txt>()
     const thirdText = createRef<Txt>()
 
+    const borderLength = createSignal(0)
+
     view.add(
         <>
-            <Txt ref={firstLetter} text={"C"} x={-200} fontSize={320} fontFamily={"Open Sans"}/>
-            <Txt ref={secondLetter} text={"P"} fontSize={320} fontFamily={"Open Sans"}/>
-            <Txt ref={thirdLetter} text={"C"} x={200} fontSize={320} fontFamily={"Open Sans"}/>
+            {/*<Txt fill={"orange"} text={"CPC"}*/}
+            {/*     fontSize={700} fontFamily={"Open Sans"}/>*/}
+
+            <Txt fill={"white"} shadowBlur={120} shadowColor={enomicsColor} ref={firstLetter} text={"C"} x={-435}
+                 fontSize={700} fontFamily={"Open Sans"}/>
+            <Txt fill={"white"} shadowBlur={120} shadowColor={enomicsColor} ref={secondLetter} text={"P"} fontSize={700}
+                 fontFamily={"Open Sans"}/>
+            <Txt fill={"white"} shadowBlur={120} shadowColor={enomicsColor} ref={thirdLetter} text={"C"} x={435}
+                 fontSize={700} fontFamily={"Open Sans"}/>
 
             <Txt ref={firstText} y={-250} x={-200} fontSize={120} fontFamily={"Open Sans"}/>
             <Txt ref={secondText} fontSize={120} fontFamily={"Open Sans"}/>
             <Txt ref={thirdText} y={250} x={200} fontSize={120} fontFamily={"Open Sans"}/>
+
+
+            <Rect shadowColor={"black"} shadowBlur={50} height={view.height()} width={() => borderLength()}
+                  fill={"black"} rotation={30} y={-view.height()}/>
+            <Rect shadowColor={"black"} shadowBlur={50} height={view.height()} width={() => borderLength()}
+                  fill={"black"} rotation={30} y={view.height()}/>
+
+            <Txt fill={"white"} fontFamily={"Open Sans"} text={"Presented by Neudorfer Niklas"} x={view.width() / -2 + 380}
+                 y={view.height() / 2 - 60}/>
         </>
     )
 
@@ -72,6 +90,9 @@ export default makeScene2D(function* (view) {
             firstLetter().position.y(-250, 1),
             secondLetter().position.y(0, 0),
             thirdLetter().position.y(250, 1),
+            firstLetter().fontSize(320, 1),
+            secondLetter().fontSize(320, 1),
+            thirdLetter().fontSize(320, 1),
 
             firstLetter().position.x(-800, 1),
             secondLetter().position.x(-200, 1),
@@ -81,9 +102,17 @@ export default makeScene2D(function* (view) {
             firstText().position.x(-450, 1),
             secondText().position.x(30, 1),
             thirdText().position.x(580, 1),
+        ),
+        all(
             firstLetter().fill(enomicsColor, 1),
             secondLetter().fill(enomicsColor, 1),
             thirdLetter().fill(enomicsColor, 1),
+
+            firstLetter().shadowBlur(10, 1),
+            secondLetter().shadowBlur(10, 1),
+            thirdLetter().shadowBlur(10, 1),
+
+            borderLength(view.width() * 2, 2)
         ),
         sequence(
             0.1,
